c31280419ac9d042cbe003175dff9bd8bbad8d1b
Yassine YBL <yassine.blila.95@gmail.com>
2024-09-10 22:29:46 +0100

    Upt simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-intereset = principal * rate * time 
-print("The simple interest is: " ,intereset)
+interest = principal * rate * time 
+print("The simple interest is:" ,interest)
