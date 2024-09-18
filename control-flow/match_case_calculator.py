38c51fd1905a405dea7985995c10b9dfaf024425
Yassine YBL <yassine.blila.95@gmail.com>
2024-09-18 21:10:14 +0100

    Add math case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+#Simple Calculator with Match Case
+num1 =int(input("Enter the first number:"))
+num2 =int(input("Enter the second number:"))
+operation = input("Choose the operation (+, -, *, /):")
+
+if operation == "+":
+    print("The result is ",num1 + num2)
+elif operation == "-":
+    print("The result is ",num1 - num2)
+elif operation == "*":
+    print("The result is ",num1 * num2)
+elif operation == "/" and num2 != 0 :
+    print("The result is ",num1 / num2)
+elif operation == "/" and num2 == 0 :
+    print("Cannot divide by zero.")
