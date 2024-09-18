2e9e44ba9cc15cf54e2ddb80b216624120e58d35
Yassine YBL <yassine.blila.95@gmail.com>
2024-09-18 22:19:57 +0100

    Update pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,10 @@
 # Drawing Patterns with Nested Loops
 row = int(input("Enter the size of the pattern:"))
-inner_rows = row 
-while row >0 :
-    print("*"*inner_rows, end="")
+inner_rows = row
+while row > 0:
+    count_rows = 1 
+    while count_rows <= inner_rows:
+        print("*", end="")
+        count_rows +=1
     row -=1
     print()
