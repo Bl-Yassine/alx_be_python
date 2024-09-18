ba7568a99bdfd0db3f98c8f23ed1062082493351
Yassine YBL <yassine.blila.95@gmail.com>
2024-09-18 21:19:44 +0100

    Upt math case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,13 +3,11 @@ num1 =int(input("Enter the first number:"))
 num2 =int(input("Enter the second number:"))
 operation = input("Choose the operation (+, -, *, /):")
 
-if operation == "+":
-    print("The result is ",num1 + num2)
-elif operation == "-":
-    print("The result is ",num1 - num2)
-elif operation == "*":
-    print("The result is ",num1 * num2)
-elif operation == "/" and num2 != 0 :
-    print("The result is ",num1 / num2)
-elif operation == "/" and num2 == 0 :
-    print("Cannot divide by zero.")
+match operation :
+    case "+": print("The result is ",num1 + num2)
+    case "*": print("The result is ",num1 * num2)
+    case "-": print("The result is ",num1 - num2)
+    case _ if operation == "/" and num2 != 0 : print("The result is ",num1 / num2)
+    case _ if operation == "/" and num2 == 0 : print("Cannot divide by zero.")
+
+    
