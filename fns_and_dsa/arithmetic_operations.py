b8ec34a9d77a2330ab4fb08fafd7282202ebe891
Yassine YBL <yassine.blila.95@gmail.com>
2024-09-28 17:37:53 +0100

    Update3 Perform operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,7 +11,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return print("You can't divide in 0")
-            else:
+            elif num2 !=0:
                 return num1/num2
                 
 
