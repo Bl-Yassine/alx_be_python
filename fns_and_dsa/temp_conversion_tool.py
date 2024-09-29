f1b2e29cbbae0e29328618bfa1d1a8ca64d890fe
Yassine YBL <yassine.blila.95@gmail.com>
2024-09-29 11:17:27 +0100

    Update Temp conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,30 +1,28 @@
-# temperature Conversion Tool
+# Temperature Conversion Tool
 
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 Temperature = input("Enter the temperature to convert: ")
-if not Temperature.isdigit():
+
+try:
+    Temperature = float(Temperature)
+except ValueError:
     print("Invalid temperature. Please enter a numeric value.")
-    
 else:
-    Weather = input("Is this temperature in Celsius or Fahrenheit? (C/F)")
+    Weather = input("Is this temperature in Celsius or Fahrenheit? (C/F) ")
 
     def convert_to_celsius(fahrenheit):
-        FtoC = (int(fahrenheit) - 32)*FAHRENHEIT_TO_CELSIUS_FACTOR
-        return print(f"{Temperature} is {FtoC} °C")
+        FtoC = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+        print(f"{fahrenheit}°F is {FtoC:.2f}°C")
 
     def convert_to_fahrenheit(celsius):
-        CtoF = (int(celsius) * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-        return print(f"{Temperature} is) {CtoF} °F")
+        CtoF = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+        print(f"{celsius}°C is {CtoF:.2f}°F")
 
-
-    if (Weather == "C"):
+    if Weather.upper() == "C":
         convert_to_fahrenheit(Temperature)
-    elif(Weather == "F"):
+    elif Weather.upper() == "F":
         convert_to_celsius(Temperature)
-
-
-
-
-
+    else:
+        print("Invalid option. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
