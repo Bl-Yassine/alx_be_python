47c7d7a2e192f7c364f1a633015f5b02b4469da5
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-05 12:54:40 +0100

    Updt bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,13 +6,14 @@ class BankAccount:
         self.account_balance = self.account_balance + amount
         
     def withdraw(self, amount):
-        if 0 < amount <= self.account_balance:
+        if amount - self.account_balance >= 0:
             self.account_balance -= amount
             return True
-        return False
+        else: False
         
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
+
         
 
 
