2ca643d2c1ea189c3a0fed292183a08f5cc7de78
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-05 11:45:51 +0100

    Updt1 bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
             print("Insufficient funds")
 
     def display_balance(self):
-        print(f"Current balance is: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
 
 
