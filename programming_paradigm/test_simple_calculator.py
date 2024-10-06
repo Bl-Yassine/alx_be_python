a39608c50f953bf301a9eebf1af1a2602acf92ea
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-06 13:23:55 +0100

    Updt test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,5 +9,11 @@ class TestClass(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(5,8),13)
     
-    def test_division(self):
+    def test_divide(self):
         self.assertEqual(self.calc.divide(4,2),2)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5,2),3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3,5), 15)
