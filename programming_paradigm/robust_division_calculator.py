efc0f372f3a0b524552cd9cd4cbd2d5f53c648b9
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-06 11:59:02 +0100

    Updt Robust Division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,8 +1,8 @@
 #Robust division Calculator with command line arguments
 
 def safe_divide(numerator, denominator):
-    numerator = float(input("Entre the numerator :"))
-    denominator = float(input("Entre the denominator :")) 
+    numerator = input("Entre the numerator :")
+    denominator = input("Entre the denominator :") 
     try :
         numerator = float(numerator)
         denominator = float(denominator)
