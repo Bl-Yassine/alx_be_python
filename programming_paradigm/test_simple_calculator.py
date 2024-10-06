f67cc041ba85bf856bf2d409c39a8b455258637c
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-06 13:12:22 +0100

    Add test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,11 @@
+import unittest
+
+from simple_calculator import SimpleCalculator
+
+class TestClass(unittest.TestCase):
+    result = add(a,b)
+    self.assertEqual(result , a+b)
+
+
+if __name__ == "__main__":
+    unittest.main()
