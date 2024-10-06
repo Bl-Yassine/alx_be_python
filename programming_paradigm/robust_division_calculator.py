2d068914e33cf29aa364b0064c3e1f8300eb4dc7
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-06 11:51:08 +0100

    Updt Robust Division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,11 +1,12 @@
 #Robust division Calculator with command line arguments
 
 def safe_divide(numerator, denominator):
-    
+    numerator = float(input("Entre the numerator :"))
+    denominator = float(input("Entre the denominator :")) 
     try :
-        numerator = float(input("Entre the numerator :"))
-        denominator = float(input("Entre the denominator :"))
-
+        numerator = float(numerator)
+        denominator = float(denominator)
+        
         try :
             numerator / denominator
         except ZeroDivisionError:
