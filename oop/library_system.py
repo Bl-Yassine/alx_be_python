aee93b7897e47d2647416c0bdab1fed2ea474c29
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-12 00:07:00 +0100

    update library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,7 +6,7 @@ class Book:
         self.author = author
         
     def __str__(self):
-        return (f"Book : {self.title} by {self.author}")
+        return f"Book : {self.title} by {self.author}"
     
 class EBook(Book):
     def __init__(self,title,author,file_size):
@@ -26,27 +26,18 @@ class Library():
         self.books = []
         
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book) 
-
         if isinstance(book, EBook):
             self.books.append(book) 
 
         elif isinstance(book, PrintBook):
-            self.books.append(book)      
+            self.books.append(book)
 
+        if isinstance(book, Book):
+            self.books.append(book)
+            
         else:
             pass
 
     def list_books(self):
         for book in self.books:
             return book
-                
-
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-Library.add_book(classic_book)
-Library.add_book(digital_novel)
-Library.add_book(paper_novel)
