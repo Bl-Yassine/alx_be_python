d1db1c7b31d51f1b035c93430cf1a9c92f8cede3
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-11 22:41:06 +0100

    Add library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,27 @@
+# Mastering Inheritance and Composition in Python
+
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+    
+class EBook:
+    def __init__(self,title,author,file_size):
+        super().__init__(title,author)
+        self.file_size = file_size
+
+class PrintBook:
+    def __init__(self,title,author,page_count):
+        super().__init__(title,author)
+        self.page_count = page_count
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self,title,author,page_count,file_size,book):
+        super().__init__(title,author,page_count,file_size)
+        self._books.append(book)
+    
+    def list_books(self):
+        print(f"the list books : {self._books}")
