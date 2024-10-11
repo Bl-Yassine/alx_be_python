b094ff4635dfef0c4da74d3675c4b5f73342e21b
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-12 00:01:46 +0100

    update library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,11 +1,12 @@
 # Mastering Inheritance and Composition in Python
 
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title, author ):
         self.title = title
         self.author = author
+        
     def __str__(self):
-        return f"Book: {self.title} by {self.author}"
+        return (f"Book : {self.title} by {self.author}")
     
 class EBook(Book):
     def __init__(self,title,author,file_size):
@@ -42,3 +43,10 @@ class Library():
             return book
                 
 
+classic_book = Book("Pride and Prejudice", "Jane Austen")
+digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+Library.add_book(classic_book)
+Library.add_book(digital_novel)
+Library.add_book(paper_novel)
