0609283e0a4bdd387f82331ffefb17abd835bd65
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-11 22:43:15 +0100

    Update Library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,17 +5,17 @@ class Book:
         self.title = title
         self.author = author
     
-class EBook:
+class EBook(Book):
     def __init__(self,title,author,file_size):
         super().__init__(title,author)
         self.file_size = file_size
 
-class PrintBook:
+class PrintBook(Book):
     def __init__(self,title,author,page_count):
         super().__init__(title,author)
         self.page_count = page_count
 
-class Library:
+class Library(Book,EBook,PrintBook):
     def __init__(self):
         self._books = []
 
