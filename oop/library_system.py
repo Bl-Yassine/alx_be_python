10c87d9ac92347499dadd311053d22307c6f1376
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-12 12:56:12 +0100

    Update Library_system

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,19 +1,17 @@
-# Mastering Inheritance and Composition in Python
+#Mastering Inheritance and composition in python
 
 class Book:
-    def __init__(self, title, author ):
+    def __init__(self,title,author):
         self.title = title
         self.author = author
-        
     def __str__(self):
-        return f"Book : {self.title} by {self.author}"
-    
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
-    def __init__(self,title,author,file_size):
+    def __init__(self,title,author, file_size):
         super().__init__(title,author)
         self.file_size = file_size
     def __str__(self):
-            return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"Ebook: {self.title} by {self.author}, File Size : {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
@@ -21,24 +19,15 @@ class PrintBook(Book):
         self.page_count = page_count
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-
-class Library():
+    
+class Library:
     def __init__(self):
-        self.books = []
-        
+        self.Books = []  # Create an empty list to store books
+
     def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book) 
-
-        if isinstance(book, EBook):
-            self.books.append(book) 
-
-        elif isinstance(book, PrintBook):
-            self.books.append(book)      
-
-        else:
-            pass
+        self.Books.append(book)
 
     def list_books(self):
-        for book in self.books:
-            return book
+        for book in self.Books:
+            print(book)
+        
