4b9765d71ac65ec23cb7efd935bf8fb05455ca09
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-12 15:18:59 +0100

    Add Polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+#Exploring Polymorphism and Method Overriding
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        print(f"The area of the Rectangle is: {self.length * self.width}")
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius 
+    
+    def area(self):
+        print(f"The area of the Circle is: {math.pi * self.radius ** 2}")
+
+
